ce81eb867e54dfeccfe183d9df48bb8b2945cacd
barasamichael <ourdigitaltimes@gmail.com>
2024-05-21 08:42:44 +0300

    Revises Main Blueprint

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, current_app
+from flask import Blueprint
+from flask import current_app
 
-main = Blueprint('main', __name__)
+main = Blueprint("main", __name__)
 from . import views, errors
+
 
 @main.app_context_processor
 def global_variables():
@@ -9,8 +11,7 @@ def global_variables():
     Provides global variables that can be accessed directly within templates
     belonging to the 'main' blueprint.
 
-    Returns:
-        dict: A dictionary containing global variables to be injected into 
-        templates.
+    :return: Dict - A dictionary containing global variables to be injected
+        into templates.
     """
-    return dict(app_name = current_app.config['ORGANIZATION_NAME'])
+    return dict(app_name=current_app.config["ORGANIZATION_NAME"])
